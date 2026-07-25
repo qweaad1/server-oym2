21ff0f2f4c42ed10cbbdf8d0fad404691e9e2123
qweaad1 <rafshan0u3@gmail.com>
2026-07-25 19:05:03 +0700

    seller

--- oymAPI/Program.cs
+++ oymAPI/Program.cs
@@ -11,25 +11,45 @@ internal class Program
     {
         try
         {
+         
             // Console.WriteLine("print(\"START\")");
             string input = args[0];
+           
 
-         
-            Match petId = Regex.Match(input, @"vID=<(\S+)>");
-            //string path = "C:\\AD\\OSPanel\\home\\example.local\\note2.txt";
-            //using StreamWriter writer = new StreamWriter(path, append: false);
-            //writer.WriteLineAsync(input);
-            int firstOpen = input.IndexOf('{');
-            int lastClose = input.LastIndexOf('}');
-            string json = "";
-            for (int i = firstOpen; i <= lastClose; i++)
+
+            Match type = Regex.Match(input, @"type=<(\S+)>");
+            if (type.Groups[1].Value == "sell")
             {
-                json += input[i];
+                Match petId = Regex.Match(input, @"vID=<(\S+)>");
+                //string path = "C:\\AD\\OSPanel\\home\\example.local\\note2.txt";
+                //using StreamWriter writer = new StreamWriter(path, append: false);
+                //writer.WriteLineAsync(input);
+                int firstOpen = input.IndexOf('{');
+                int lastClose = input.LastIndexOf('}');
+                string json = "";
+                for (int i = firstOpen; i <= lastClose; i++)
+                {
+                    json += input[i];
+                }
+                 Console.WriteLine(Sell.add(json.Replace(" ", "\"") ));
+
             }
-           string summerEvent = "for i = 1, 4  do \r\ngame:GetService(\"ReplicatedStorage\").adoptme_new_net[\"adoptme_legacy_shared.ContentPacks.Summer2026.Game.BuriedTreasure.BuriedTreasureNet:23\"]:InvokeServer()\r\nwait(3)\r\ngame:GetService(\"ReplicatedStorage\").adoptme_new_net[\"adoptme_legacy_shared.ContentPacks.Summer2026.Game.BuriedTreasure.BuriedTreasureNet:6\"]:InvokeServer()\r\nend";
-            summerEvent = "";
-          Console.WriteLine(ailments.getExecute(json.Replace(" ", "\""), petId.Groups[1].Value) + "\r\n wait(60) \r\n"+ summerEvent + "\r\n loadstring(game:HttpGet(\"http://192.168.3.4/loader.txt\"))()");
-            
+            if (type.Groups[1].Value != "sell"){
+                Match petId = Regex.Match(input, @"vID=<(\S+)>");
+               
+                int firstOpen = input.IndexOf('{');
+                int lastClose = input.LastIndexOf('}');
+                string json = "";
+                for (int i = firstOpen; i <= lastClose; i++)
+                {
+                    json += input[i];
+                }
+                string summerEvent = "for i = 1, 4  do \r\ngame:GetService(\"ReplicatedStorage\").adoptme_new_net[\"adoptme_legacy_shared.ContentPacks.Summer2026.Game.BuriedTreasure.BuriedTreasureNet:23\"]:InvokeServer()\r\nwait(3)\r\ngame:GetService(\"ReplicatedStorage\").adoptme_new_net[\"adoptme_legacy_shared.ContentPacks.Summer2026.Game.BuriedTreasure.BuriedTreasureNet:6\"]:InvokeServer()\r\nend";
+                summerEvent = "";
+                Console.WriteLine(ailments.getExecute(json.Replace(" ", "\""), petId.Groups[1].Value) + "\r\n wait(60) \r\n" + summerEvent + "\r\n loadstring(game:HttpGet(\"http://192.168.3.4/loader.txt\"))()");
+
+            }
+
         }
         catch (Exception value)
         {
---
+++ oymAPI/Sell.cs
@@ -0,0 +1,80 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace oymAPI
+{
+    public static class Sell
+    {
+        public static string add(string jsonr, string namePlayer)
+        {
+
+            try
+            {
+                string json = $"{{\"obj\":[ {jsonr}]}}";
+                string path = "C:\\AD\\OSPanel\\home\\example.local\\note4.txt";
+                using StreamWriter writer = new StreamWriter(path, append: false);
+                writer.WriteLineAsync(json);
+
+
+                // Строка подключения к MySQL
+                string connectionString = "Server=localhost;Database=qweaad;Uid=root;Pwd=your_password;";
+
+                using JsonDocument doc = JsonDocument.Parse(json);
+                JsonElement root = doc.RootElement;
+
+                // Получаем массив obj
+                JsonElement objArray = root.GetProperty("obj");
+
+                using MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+                foreach (JsonElement item in objArray.EnumerateArray())
+                {
+                     
+                    string type = item.GetProperty("type").GetString();
+                    string kind = item.GetProperty("kind").GetString();
+                    string age = item.GetProperty("age").GetString();
+                    
+                    string id = item.GetProperty("id").GetString();
+                    int index = item.GetProperty("index").GetInt32();
+
+                   
+                    string owner = "player1";
+                    string rare = "common";
+
+                    // SQL запрос
+                    string sql = @"INSERT INTO `qweaad`.`inventory` 
+                   (`id`, `id_string`, `name`, `type`, `age`, `owner`, `rare`) 
+                   VALUES (@id, @id_string, @name, @type, @age, @owner, @rare)";
+
+                    using MySqlCommand cmd = new MySqlCommand(sql, connection);
+                    cmd.Parameters.AddWithValue("@id", index);
+                    cmd.Parameters.AddWithValue("@id_string", id);
+                    cmd.Parameters.AddWithValue("@name", "NULL");
+                    cmd.Parameters.AddWithValue("@type", kind);
+                    cmd.Parameters.AddWithValue("@age", age);
+                    cmd.Parameters.AddWithValue("@owner", namePlayer);
+                    cmd.Parameters.AddWithValue("@rare", "NULL");
+
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine($"Inserted: {kind} (index: {index})");
+                }
+
+                connection.Close();
+                Console.WriteLine("Done!");
+
+            }
+            catch (Exception value)
+            {
+                Console.WriteLine(($"warn(\"{value}\")").Replace("\r\n"," ").Replace("\r", " "));
+            }
+            return "print 1";
+        }
+    }
+}
+
